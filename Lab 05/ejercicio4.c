2903c93b74ed7db26847eb50108e9c04e5b5e035
Brayan Fajardo <brayan.f.a07@hotmail.com>
2017-09-19 15:28:50 -0600

    Cambios iniciales realizados en ejercicio3.c y ejercicio4.c

---
+++ Lab 05/ejercicio4.c
@@ -0,0 +1,28 @@
+//ejercicio4.c
+
+#include "malloc.h"
+int main() {
+	int* arreglo;
+	arreglo = (int*) malloc(sizeof(int)*10);
+
+	for(int i = 0; i<10; i++){
+		arreglo[i] = 10*i;
+	}
+
+	for(int i = 0; i < 10; i++){
+		printf("%i\n",arreglo[i]);
+	}
+
+	free((char*)arreglo);
+
+
+	arreglo = (int*)malloc(sizeof(int)*10);
+
+	for (int i = 0; i<10; i++){
+		arreglo[i]=20*i;
+	}
+
+	for(int i = 0; i<10; i++){
+		printf("%i\n",arreglo[i]);
+	}
+}
