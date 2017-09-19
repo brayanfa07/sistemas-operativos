e1c82be8c2832a2103962d062fcb83c674699c7a
Brayan Fajardo <brayan.f.a07@hotmail.com>
2017-09-19 15:45:23 -0600

    Cambios realizados en ejercicio1.c

--- Lab 05/alloc.h
+++ Lab 05/alloc.h
@@ -6,21 +6,44 @@ static char allocbuf[ALLOCSIZE]; // almacenamiento para alloc
 static char *allocp = allocbuf; // siguiente posición libre
 
 
-char *alloc(int n) { // regresa un puntero a n caracteres
-	for (int i=0; i<1000; i++){
-		//printf("Printing allocbuf %i\n",allocbuf[i]);
+char *alloc(int n, int i) { // regresa un puntero a n caracteres
+	int memorySector = i;
+	if (memorySector == 1){
+		for (int i=0; i<1000; i++){
+			//printf("Printing allocbuf %i\n",allocbuf[i]);
+		}
+		printf("Numero allocsize %d\n", ALLOCSIZE);
+		printf("Numero allocp %d\n", *allocp);
+		if (allocbuf + ALLOCSIZE - allocp >= n) {
+			allocp += n;
+			printf("Print de N %d\n", n);
+			printf("Print de allocp 1 %d\n", *allocp);
+			printf("Print de allocp 2 (allocp - n) %d\n", *allocp -n);
+			return allocp -n;
+		} 
+		else{
+			return 0;
+		}
+		
+	}else{
+		if (memorySector == 2){
+			for (int i=0; i<1000; i++){
+			//printf("Printing allocbuf %i\n",allocbuf[i]);
+			}
+			printf("Numero allocsize %d\n", ALLOCSIZE);
+			printf("Numero allocp %d\n", *allocp);
+			if (allocbuf + ALLOCSIZE - allocp >= n) {
+				*allocp = n;
+				printf("Print de N %d\n", n);
+				printf("Print de allocp 1 %d\n", *allocp);
+				printf("Print de allocp 2 (allocp - n) %d\n", *allocp -n);
+				return allocp -n;
+			} else
+				return 0;
+		}
 	}
-	printf("Numero allocsize %d\n", ALLOCSIZE);
-	printf("Numero allocp %d\n", *allocp);
-	if (allocbuf + ALLOCSIZE - allocp >= n) {
-		allocp += n;
-		printf("Print de N %d\n", n);
-		printf("Print de allocp 1 %d\n", *allocp);
-		printf("Print de allocp 2 (allocp - n) %d\n", *allocp -n);
-		return allocp -n;
-	} else
-		return 0;
-	}
+}
+
 	
 void afree(char *p) { // libera la memoria apunta por p
 	if (p>= allocbuf && p < allocbuf + ALLOCSIZE)
--- Lab 05/ejercicio1.c
+++ Lab 05/ejercicio1.c
@@ -3,14 +3,14 @@
 
 int main() {
 	int* arreglo;
-	arreglo = (int*)alloc(sizeof(int)*10);
+	arreglo = (int*)alloc(sizeof(int)*10, 2); //Ingresa 1 o 2 dependiendo de donde se quiere guardar
 	for (int i=0; i<10; i++)
 		arreglo[i]=10*i;
 
 		for (int i=0; i<10; i++)
 			printf("%i\n",arreglo[i]);
 			afree((char*)arreglo);
-			arreglo = (int*)alloc(sizeof(int)*10);
+			arreglo = (int*)alloc(sizeof(int)*10, 2);
 
 		for (int i=0; i<10; i++)
 			arreglo[i]=20*i;
